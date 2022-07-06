71c3529a2cdc35bc2058afa45e81517c01a9c678
PetroSuch <p.such@nltu.lviv.ua>
2022-07-06 12:00:18 +0300

    test sql

---
+++ VersionSQL/LAPTOP-JF3A19B0/master/dbo/Procedure/FindCustomerByName.sql
@@ -0,0 +1,9 @@
+﻿/****** Object:  Procedure [dbo].[FindCustomerByName]    Committed by VersionSQL https://www.versionsql.com ******/
+
+ 
+CREATE PROCEDURE dbo.FindCustomerByName
+    @customer_name nvarchar(50) 
+AS   
+    SELECT *  
+    FROM dbo.customers3  
+    WHERE customer_name = @customer_name;  
